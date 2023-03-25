6a3e26a6bb511c17728bdcc683a5e06ce658fc51
ApurvZ <43213555+ApurvZ@users.noreply.github.com>
2023-03-25 23:16:41 +0530

    Add files via upload

---
+++ haydn/AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_haydn.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_haydn-user \
+    aosp_haydn-userdebug \
+    aosp_haydn-eng
---
+++ haydn/aosp_haydn.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from haydn device
+$(call inherit-product, device/xiaomi/haydn/device.mk)
+
+# Inherit some common AOSP/PE stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := haydn
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := M2012K11G
+PRODUCT_NAME := aosp_haydn
+
+# AOSP/PE
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_EXCLUDES_AUDIOFX := true
+USE_PIXEL_CHARGER := true
+
+PRODUCT_SYSTEM_NAME := haydn_global
+PRODUCT_SYSTEM_DEVICE := haydn
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="haydn_global-user 12 SKQ1.211006.001 V13.0.4.0.SKKMIXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := Xiaomi/haydn_global/haydn:12/SKQ1.211006.001/V13.0.4.0.SKKMIXM:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
